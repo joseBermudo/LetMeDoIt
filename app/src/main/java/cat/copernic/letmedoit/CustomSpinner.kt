4cba6e4cf1b656f61694214de5dbd0d31a7313ee
=Alexandru Cruceat <=cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-10-13 12:02:43 +0200

    Corrección de errores

--- app/src/main/java/cat/copernic/letmedoit/CustomSpinner.kt
+++ app/src/main/java/cat/copernic/letmedoit/CustomSpinner.kt
@@ -2,26 +2,32 @@ package cat.copernic.letmedoit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import cat.copernic.letmedoit.databinding.ActivityCustomspinnerBinding
 
-class CustomSpinner : AppCompatActivity() {
+class CUSTOMSPINNER : AppCompatActivity() {
     private lateinit var binding: ActivityCustomspinnerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCustomspinnerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
 
 
         //EJEMPLO DE USO DEL SPINNER --> HAY QUE IMPLEMENTARLO SI NO NO IRÁ
-        val spinner = binding.spinner
+        val spinner = binding.spinnerSubcategory
 
         var list: ArrayList<String> = ArrayList<String>()
         list.add("A")
         list.add("B")
         list.add("C")
 
-        Utils.AsignarPopUpSpinner(this,list,spinner)
+        CrearSpinner(list, spinner)
+    }
 
-        //
+    private fun CrearSpinner(list : ArrayList<String>, spinner : Spinner){
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.spinner_items,list)
+        spinner.adapter  = adapter
     }
 }
