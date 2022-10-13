c3879018351b6401952c9d5a9afd60fa25f47cf9
=Jose Antonio Bermudo Olea <=bermudo.olea.jose.antonio@alumnat.copernic.cat>
2022-10-13 11:51:53 +0200

    Diselar filtro categorias

---
+++ app/src/main/java/cat/copernic/letmedoit/Users/FiltroCategoriasTest.kt
@@ -0,0 +1,12 @@
+package cat.copernic.letmedoit.Users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.copernic.letmedoit.R
+
+class FiltroCategoriasTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtro_categorias_test)
+    }
+}
