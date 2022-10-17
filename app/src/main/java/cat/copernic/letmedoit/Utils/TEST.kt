13bdb6c08f6f77ff86c27a5030d17b6aeb7da8f3
=Alexandru Cruceat <=cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-10-17 21:25:33 +0200

    Organización del proyecto

---
+++ app/src/main/java/cat/copernic/letmedoit/Utils/TEST.kt
@@ -0,0 +1,12 @@
+package cat.copernic.letmedoit.Utils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.copernic.letmedoit.R
+
+class TEST : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
