9db522af8c1d51bdfb02f32e692ccf7da112cfa8
=Jose Antonio Bermudo Olea <=bermudo.olea.jose.antonio@alumnat.copernic.cat>
2022-10-27 20:50:49 +0200

    Recyvle view lista categorias admin 8

--- app/src/main/java/cat/copernic/letmedoit/General/model/adapter/AdminCategoryViewHolder.kt
+++ app/src/main/java/cat/copernic/letmedoit/General/model/adapter/AdminCategoryViewHolder.kt
@@ -1,22 +1,31 @@
 package cat.copernic.letmedoit.General.model.adapter
 
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.letmedoit.General.model.Category
-import cat.copernic.letmedoit.R
 import cat.copernic.letmedoit.databinding.ItemListCategoryAdminBinding
-import com.bumptech.glide.Glide
 
-class AdminCategoryViewHolder(val binding: ItemListCategoryAdminBinding) : RecyclerView.ViewHolder(binding.root) {
+class AdminCategoryViewHolder(val binding: ItemListCategoryAdminBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     val categoryIcon = binding.imgCategory
     val categoryName = binding.txtCategoryName
-    fun render(categoryModel: Category, onClickListener:(Category) -> Unit){
-        categoryIcon.background= ContextCompat.getDrawable(binding.root.context,binding.root.resources.getIdentifier(categoryModel.image, "drawable","cat.copernic.letmedoit"))
-        categoryName.text = categoryModel.nombre.replace(" ","\n")
+
+    fun render(
+        categoryModel: Category,
+        onClickListener: (Category) -> Unit,
+        onClickDelete: (Int) -> Unit
+    ) {
+        categoryIcon.background = ContextCompat.getDrawable(
+            binding.root.context,
+            binding.root.resources.getIdentifier(
+                categoryModel.image,
+                "drawable",
+                "cat.copernic.letmedoit"
+            )
+        )
+        categoryName.text = categoryModel.nombre.replace(" ", "\n")
         itemView.setOnClickListener { onClickListener(categoryModel) }
+        binding.itemCategoryDeleteIcon.setOnClickListener { onClickDelete(absoluteAdapterPosition) }
     }
 }
--- app/src/main/java/cat/copernic/letmedoit/General/view/fragments/AdminCategoriesList.kt
+++ app/src/main/java/cat/copernic/letmedoit/General/view/fragments/AdminCategoriesList.kt
@@ -1,20 +1,22 @@
 package cat.copernic.letmedoit.General.view.fragments
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.letmedoit.General.model.Category
 import cat.copernic.letmedoit.General.model.CategoryProvider
+import cat.copernic.letmedoit.General.model.Subcategory
 import cat.copernic.letmedoit.General.model.adapter.AdminCategoryAdapter
-import cat.copernic.letmedoit.General.model.adapter.CategoryAdapter
 import cat.copernic.letmedoit.R
 import cat.copernic.letmedoit.databinding.FragmentAdminCategoriesListBinding
+import com.google.android.material.textfield.TextInputEditText
 
 
 class AdminCategoriesList : Fragment() {
@@ -22,6 +24,11 @@ class AdminCategoriesList : Fragment() {
     private val binding get() = _binding!!
     private var recyclerGrid = false
     private lateinit var recyclerView: RecyclerView
+    private var categoryMutableList: MutableList<Category> =
+        CategoryProvider.obtenerCategorias().toMutableList()
+    private lateinit var adapter: AdminCategoryAdapter
+    private lateinit var llmanager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,41 +38,81 @@ class AdminCategoriesList : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       _binding = FragmentAdminCategoriesListBinding.inflate(inflater,container, false)
+        _binding = FragmentAdminCategoriesListBinding.inflate(inflater, container, false)
+        llmanager = LinearLayoutManager(binding.root.context)
         initRecyclerView()
         recyclerView = binding.rcvListaCategorias
         binding.btnRecyclerMode.setOnClickListener {
             selectLayoutReyclerMode()
 
         }
+        binding.btnAdd.setOnClickListener { createCategory() }
         return binding.root
     }
 
-    private fun selectLayoutReyclerMode(){
-        if(recyclerGrid){
+    private fun createCategory() {
+
+        val dialogBinding = layoutInflater.inflate(R.layout.create_category_dialog, null)
+        val myDialog = Dialog(binding.root.context)
+        myDialog.setContentView(dialogBinding)
+        myDialog.setCancelable(true)
+        myDialog.show()
+
+        val btn_cancel = dialogBinding.findViewById<Button>(R.id.btn_cancelCreateCategorie)
+        btn_cancel.setOnClickListener {
+            myDialog.dismiss()
+        }
+
+        val btn_done = dialogBinding.findViewById<Button>(R.id.btn_doneCreate)
+
+        btn_done.setOnClickListener {
+            val txtInput_name =
+                dialogBinding.findViewById<TextInputEditText>(R.id.txtInput_categoryName)
+            val name = txtInput_name.text.toString().trim()
+            if (!name.isEmpty() && !name.isBlank()) {
+                val category = Category(
+                    name,
+                    Subcategory("Pasear perros", "100"),
+                    "favorites_icon",
+                    "3"
+                )
+                categoryMutableList.add(index = 0, category)
+                adapter.notifyItemInserted(0)
+                llmanager.scrollToPositionWithOffset(0, 10)
+                myDialog.dismiss()
+            }
+        }
+
+
+    }
+
+    private fun selectLayoutReyclerMode() {
+        if (recyclerGrid) {
             recyclerGrid = false
-            recyclerView.layoutManager = GridLayoutManager(binding.root.context,3)
-            binding.btnRecyclerMode.setBackgroundResource(R.drawable.ic_baseline_format_list_bulleted_24)
-        }else{
+            binding.btnRecyclerMode.setIconResource(R.drawable.ic_baseline_format_list_bulleted_24)
+        } else {
             recyclerGrid = true
-            recyclerView.layoutManager = LinearLayoutManager(binding.root.context)
-            binding.btnRecyclerMode.setBackgroundResource(R.drawable.ic_baseline_grid_view_24)
+            binding.btnRecyclerMode.setIconResource(R.drawable.ic_baseline_grid_view_24)
 
         }
     }
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         //val manager = GridLayoutManager(binding.root.context,2)
-
-        binding.rcvListaCategorias.layoutManager = LinearLayoutManager(binding.root.context)
-        binding.rcvListaCategorias.adapter = AdminCategoryAdapter(CategoryProvider.obtenerCategorias()) { category ->
-            onItemSelected(
-                category
-            )
-        }
+        adapter = AdminCategoryAdapter(
+            categoryList = categoryMutableList,
+            onClickListener = { category -> onItemSelected(category) },
+            onClickDelete = { position -> onDeletedItem(position) })
+        binding.rcvListaCategorias.layoutManager = llmanager
+        binding.rcvListaCategorias.adapter = adapter
     }
 
-    fun onItemSelected(category: Category){
-        Toast.makeText(binding.root.context,category.nombre,Toast.LENGTH_SHORT).show()
+    private fun onItemSelected(category: Category) {
+        Toast.makeText(binding.root.context, category.nombre, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onDeletedItem(position: Int) {
+        categoryMutableList.removeAt(position)
+        adapter.notifyItemRemoved(position)
     }
 }
