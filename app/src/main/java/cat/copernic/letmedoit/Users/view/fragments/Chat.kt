8dd1ea7913a10a1dfdda787fe578cae574770513
=Hugo Barrera <=barrera.castillo.hugo@alumnat.copernic.cat>
2022-11-07 13:58:53 +0100

    Chat con navegacion

--- app/src/main/java/cat/copernic/letmedoit/Users/view/fragments/Chat.kt
+++ app/src/main/java/cat/copernic/letmedoit/Users/view/fragments/Chat.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cat.copernic.letmedoit.R
+import cat.copernic.letmedoit.databinding.FragmentChatBinding
+import cat.copernic.letmedoit.databinding.FragmentFiltroCategoriasBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +20,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class chat : Fragment() {
+
+    lateinit var binding : FragmentChatBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +39,10 @@ class chat : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+
+        binding = FragmentChatBinding.inflate(inflater, container, false)
+        binding.btnArrowBack.setOnClickListener { requireActivity().onBackPressed() }
+        return binding.root
     }
 
     companion object {
