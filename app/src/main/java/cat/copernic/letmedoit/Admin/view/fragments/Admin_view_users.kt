ee1fded348509766a71c18d4b8b8631c8d3e616c
=Hugo Barrera <=barrera.castillo.hugo@alumnat.copernic.cat>
2022-11-10 12:16:27 +0100

    resolv conflicts

---
+++ app/src/main/java/cat/copernic/letmedoit/Admin/view/fragments/Admin_view_users.kt
@@ -0,0 +1,44 @@
+
+package cat.copernic.letmedoit.Admin.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import cat.copernic.letmedoit.General.model.UsersProvider
+import cat.copernic.letmedoit.General.model.adapter.UsersAdapter
+import cat.copernic.letmedoit.databinding.FragmentAdminViewUsersBinding
+
+
+class admin_view_users : Fragment() {
+
+    private var _binding:FragmentAdminViewUsersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAdminViewUsersBinding.inflate(inflater, container, false)
+        initRecyclerView()
+
+        binding.btnFlechaBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+
+        return binding.root
+    }
+
+    fun initRecyclerView(){
+        binding.recyclerViewUsers.layoutManager = LinearLayoutManager(binding.root.context)
+        binding.recyclerViewUsers.adapter = UsersAdapter(UsersProvider.obtenerUsers())
+    }
+
+}
