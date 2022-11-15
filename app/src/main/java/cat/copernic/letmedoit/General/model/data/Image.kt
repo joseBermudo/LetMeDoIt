92310b0ec52fdd3bae6797744ee5692d4a87810d
Alexandru Cruceat <cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-11-15 13:50:49 +0100

    New Service Recycler Imagenes añadir, borrar seleccionar

--- app/src/main/java/cat/copernic/letmedoit/General/model/data/Image.kt
+++ app/src/main/java/cat/copernic/letmedoit/General/model/data/Image.kt
@@ -2,5 +2,6 @@ package cat.copernic.letmedoit.General.model.data
 
 data class Image (
         val id : String,
-        val img_link : String
+        val img_link : String,
+        var checked : Boolean? = false
 )
--- app/src/main/java/cat/copernic/letmedoit/Users/view/fragments/NewService.kt
+++ app/src/main/java/cat/copernic/letmedoit/Users/view/fragments/NewService.kt
@@ -1,12 +1,27 @@
 package cat.copernic.letmedoit.Users.view.fragments
 
+import android.Manifest
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Selection.selectAll
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import cat.copernic.letmedoit.R
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cat.copernic.letmedoit.General.model.adapter.ImagesAdapter
+import cat.copernic.letmedoit.General.model.data.Image
 import cat.copernic.letmedoit.databinding.FragmentNewServiceBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,8 +44,16 @@ class NewService : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        getContent = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList -> managePhotosUri(uriList) }
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permissions -> checkPermissions(permissions) }
     }
+    var colum = arrayOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        READ_EXTERNAL_STORAGE
+    )
 
+
+    lateinit var getContent : ActivityResultLauncher<String>
     lateinit var binding : FragmentNewServiceBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,7 +61,78 @@ class NewService : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentNewServiceBinding.inflate(inflater,container,false)
+        binding.btnAddImage.setOnClickListener { addImage() }
+        binding.btnRemoveImage.setOnClickListener { removeImage() }
+        binding.selectAll.setOnClickListener { selectAll() }
+        initRecyclerView()
         return binding.root
+    }
+
+    private fun selectAll() {
+        adapter.selectAll()
+    }
+
+    private fun removeImage() {
+        adapter.removeItems()
+        binding.selectAll.isChecked = false
+
+    }
+
+    private fun managePhotosUri(uriList: List<Uri>?) {
+        var num = 0
+        uriList?.forEach {
+            imagesList.add(Image(num.toString(),it.toString(),false))
+            num++
+        }
+        adapter.notifyDataSetChanged()
+    }
+
+    var storagePermission = false
+    var cameraPermission = false
+    private fun checkPermissions(permissions: Map<String, @JvmSuppressWildcards Boolean>) {
+        permissions.forEach{
+            actionMap -> when(actionMap.key){
+                CAMERA -> { cameraPermission = actionMap.value }
+                READ_EXTERNAL_STORAGE ->  { storagePermission = actionMap.value }
+            }
+        }
+    }
+
+    private fun addImage() {
+        if((ActivityCompat.checkSelfPermission( requireContext(),colum[0])!= PackageManager.PERMISSION_GRANTED) &&
+            (ActivityCompat.checkSelfPermission( requireContext(),colum[1])!= PackageManager.PERMISSION_GRANTED)){
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requireActivity().requestPermissions(colum,123)
+            }
+        }
+        else{
+            cameraPermission = true
+            storagePermission = true
+        }
+        if(cameraPermission && storagePermission){
+            openGallery()
+        }
+
+    }
+
+    private fun openGallery() {
+        getContent.launch("image/*")
+    }
+
+
+    lateinit var imagesRecyclerView : RecyclerView
+    lateinit var adapter : ImagesAdapter
+    var imagesList = ArrayList<Image>()
+    private fun initRecyclerView() {
+        imagesRecyclerView = binding.listImages
+        //LinearLayoutManager HORIZONTAL
+        //serviceRecyclerView.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL,false)
+        imagesRecyclerView.layoutManager = GridLayoutManager(binding.root.context, 2)
+        //Asignación del adaptador al recyclerview.
+
+        adapter = ImagesAdapter(imagesList)
+        imagesRecyclerView.adapter = adapter
     }
 
     companion object {
