57087a1f10ce8426132380979bf700a5ea8aaf21
Alexandru Cruceat <cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-11-17 09:19:02 +0100

    FireBaseModule Añadidos documentos

--- app/src/main/java/cat/copernic/letmedoit/Utils/di/FirebaseModule.kt
+++ app/src/main/java/cat/copernic/letmedoit/Utils/di/FirebaseModule.kt
@@ -38,8 +38,76 @@ object FirebaseModule {
         return firestore.collection(Constants.USERS_COLLECTION)
     }
 
+    @ServiceCollection
+    @Provides
+    @Singleton
+    fun serviceProvider(firestore: FirebaseFirestore) : CollectionReference{
+        return firestore.collection(Constants.SERVICES_COLLECTION)
+    }
+
+    @CategoryCollection
+    @Provides
+    @Singleton
+    fun categoryProvider(firestore: FirebaseFirestore) : CollectionReference{
+        return firestore.collection(Constants.CATEGORIES_COLLECTION)
+    }
+
+    @ReportsCollection
+    @Provides
+    @Singleton
+    fun reportsProvider(firestore: FirebaseFirestore) : CollectionReference{
+        return firestore.collection(Constants.REPORTS_COLLECTION)
+    }
+
+    @ReportsReasonCollection
+    @Provides
+    @Singleton
+    fun reasonReportsCollection(firestore: FirebaseFirestore) : CollectionReference{
+        return firestore.collection(Constants.REASONS_REPORTS_COLLECTION)
+    }
+
+    @DealCollection
+    @Provides
+    @Singleton
+    fun dealCollection(firestore: FirebaseFirestore) : CollectionReference{
+        return firestore.collection(Constants.DEALS_COLLECTION)
+    }
+
+    @ChatCollection
+    @Provides
+    @Singleton
+    fun chatCollection(firestore: FirebaseFirestore) : CollectionReference{
+        return firestore.collection(Constants.CHATS_COLLECTION)
+    }
+
     //Annotation Class --> Utilizada para identificar un binding especifico para un tipo cuandoe ste tipo tiene multiples bindings definidos
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
     annotation class UsersCollection
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class ServiceCollection
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class CategoryCollection
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class ReportsCollection
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class ReportsReasonCollection
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class DealCollection
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class ChatCollection
+
+
 }
