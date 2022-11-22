aebb371a3a049e40788d627d922125bc2924cf17
Alexandru Cruceat <cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-11-22 10:58:14 +0100

    Corrección de Errores

--- app/src/main/java/cat/copernic/letmedoit/Users/model/data/Deal.kt
+++ app/src/main/java/cat/copernic/letmedoit/Users/model/data/Deal.kt
@@ -8,5 +8,5 @@ data class Deal(
     val Services :          ServicesMap,
     val description:        String,
     val accepted :          Boolean,
-    val conclude:           Byte
+    val conclude:           Boolean
 )
